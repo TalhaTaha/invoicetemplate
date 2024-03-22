b34ff7ea8da1009caf029ea89beb6c9f0f47a829
Talha Taha <bsef20m024@pucit.edu.pk>
2024-03-22 05:42:26 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateInvoice from "./pages/CreateInvoice";
+import Invoice from "./pages/Invoice";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<BrowserRouter>
+
+  <Routes>
+    <Route path="/" element={< Home/>}></Route>
+    <Route path="/invoice" element={< Invoice/>}></Route>
+    <Route path="/create-invoice" element={<CreateInvoice/>}></Route>
+  </Routes>
+
+</BrowserRouter>
   );
 }
 
